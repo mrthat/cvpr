df2d65da83eeb26f33bb49e97b04a997d12dd91b
eifye <eifye_lilie-bear@softbank.ne.jp>
2015-05-18 10:39:13 +0900

    あげ忘れ すごい不幸になった

---
+++ cvpr/ml/base/header/PredictionParameter.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <memory>
+
+namespace cvpr
+{
+	/**
+	*	予測パラメータベースクラス
+	*/
+	class PredictionParameter
+	{
+		public:
+		protected:
+	};
+
+	typedef std::shared_ptr<PredictionParameter> PtrPredictionParameter;
+
+};
